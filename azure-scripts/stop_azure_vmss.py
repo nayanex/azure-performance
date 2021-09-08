bd92e550b3b66b7073e7fc937e1c68a83d5e219f
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2021-09-08 23:02:57 +0200

    Runbook - submission screenshots

--- azure-scripts/stop_azure_vmss.py
+++ azure-scripts/stop_azure_vmss.py
@@ -30,21 +30,22 @@ import azure.mgmt.resource
 import azure.mgmt.storage
 import azure.mgmt.compute
 import automationassets
+import OpenSSL
+from msrestazure import azure_active_directory
+import adal
 
 # Max number of VMSSs to process at a time
 _MAX_THREADS = 20
 
 # Returns a credential based on an Azure Automation RunAs connection dictionary
 def get_automation_runas_credential(runas_connection):
-    """ Returs a credential that can be used to authenticate against Azure resources """
-    from OpenSSL import crypto
-    from msrestazure import azure_active_directory
-    import adal
-
+    """Returs a credential that can be used to authenticate against Azure resources"""
     # Get the Azure Automation RunAs service principal certificate
     cert = automationassets.get_automation_certificate("AzureRunAsCertificate")
-    sp_cert = crypto.load_pkcs12(cert)
-    pem_pkey = crypto.dump_privatekey(crypto.FILETYPE_PEM, sp_cert.get_privatekey())
+    sp_cert = OpenSSL.crypto.load_pkcs12(cert)
+    pem_pkey = OpenSSL.crypto.dump_privatekey(
+        OpenSSL.crypto.FILETYPE_PEM, sp_cert.get_privatekey()
+    )
 
     # Get run as connection information for the Azure Automation service principal
     application_id = runas_connection["ApplicationId"]
@@ -57,30 +58,37 @@ def get_automation_runas_credential(runas_connection):
     context = adal.AuthenticationContext(authority_url)
     return azure_active_directory.AdalAuthentication(
         lambda: context.acquire_token_with_client_certificate(
-            resource,
-            application_id,
-            pem_pkey,
-            thumbprint)
+            resource, application_id, pem_pkey, thumbprint
+        )
     )
 
+
 class StopVMSSThread(threading.Thread):
-    """ Thread class to stop Azure VMSS """
+    """Thread class to stop Azure VMSS"""
+
     def __init__(self, resource_group, vmss_name):
         threading.Thread.__init__(self)
         self.resource_group = resource_group
         self.vmss_name = vmss_name
+
     def run(self):
-        print "Stopping " + self.vmss_name + " in resource group " + self.resource_group
+        print(
+            "Stopping " + self.vmss_name + " in resource group " + self.resource_group
+        )
         sys.stdout.flush()
         stop_vmss(self.resource_group, self.vmss_name)
-        print "Stopped " + self.vmss_name + " in resource group " + self.resource_group
+        print("Stopped " + self.vmss_name + " in resource group " + self.resource_group)
         sys.stdout.flush()
 
+
 def stop_vmss(resource_group, vmss_name):
-    """ Stops a vm in the specified resource group """
-    # Stop the VM
-    vmss_stop = compute_client.virtual_machine_scale_sets.deallocate(resource_group, vmss_name)
+    """Stops a vm in the specified resource group"""
+    # Stop the VMSS
+    vmss_stop = compute_client.virtual_machine_scale_set_vms_vms.begin_deallocate(
+        resource_group, vmss_name
+    )
     vmss_stop.wait()
+
 
 # Process any arguments sent in
 resource_group_name = None
@@ -88,9 +96,9 @@ vmss_name = None
 
 opts, args = getopt.getopt(sys.argv[1:], "g:v:")
 for o, a in opts:
-    if o == '-g':  # if resource group name is passed with -g option then take it
+    if o == "-g":  # if resource group name is passed with -g option then take it
         resource_group_name = a
-    elif o == '-v':  # if vm name is mentioned after script name with -v then read it
+    elif o == "-v":  # if vm name is mentioned after script name with -v then read it
         vmss_name = a
 
 # Check for correct arguments passed in
@@ -98,16 +106,19 @@ if vmss_name is not None and resource_group_name is None:
     raise ValueError("VMSS argument passed in without a resource group specified")
 
 # Authenticate to Azure using the Azure Automation RunAs service principal
-automation_runas_connection = automationassets.get_automation_connection("AzureRunAsConnection")
+automation_runas_connection = automationassets.get_automation_connection(
+    "AzureRunAsConnection"
+)
 azure_credential = get_automation_runas_credential(automation_runas_connection)
 subscription_id = str(automation_runas_connection["SubscriptionId"])
 
 resource_client = azure.mgmt.resource.ResourceManagementClient(
-    azure_credential,
-    subscription_id)
+    azure_credential, subscription_id
+)
 
 compute_client = azure.mgmt.compute.ComputeManagementClient(
-    azure_credential, subscription_id)
+    azure_credential, subscription_id
+)
 
 # Get list of resource groups
 groups = []
@@ -120,8 +131,10 @@ elif resource_group_name is not None and vmss_name is None:
     groups.append(resource_group)
 elif resource_group_name is not None and vmss_name is not None:
     # Specific resource group and VMSS name passed in so stop the VMSS.
-    vmss_detail = compute_client.virtual_machine_scale_sets.get(resource_group_name, vmss_name, expand='instanceView')
-    if vmss_detail.get_instance_view.statuses[1].code == 'PowerState/running':
+    vmss_detail = compute_client.virtual_machine_scale_set_vms.get(
+        resource_group_name, vmss_name, expand="instanceView"
+    )
+    if vmss_detail.get_instance_view.statuses[1].code == "PowerState/running":
         stop_vmss(resource_group_name, vmss_name)
 
 # List of threads that are used to Stop VMSSs in parallel
@@ -129,21 +142,23 @@ vmss_threads_list = []
 
 # Process any VMs that are in a group
 for group in groups:
-    vmss_list = compute_client.virtual_machine_scale_sets.list(group.name)
+    vmss_list = compute_client.virtual_machine_scale_set_vms.list(group.name, "udacity-vmss")
     for vmss in vmss_list:
-        vmss_detail = compute_client.virtual_machine_scale_sets.get(group.name, vmss.name, expand=None)
-        if type(vmss_detail) == "VirtualMachineScaleSet":
-            if vmss_detail.get_instance_view.statuses[1].code == 'PowerState/running':
-                stop_vmss_thread = StopVMSSThread(group.name, vmss.name)
-                stop_vmss_thread.start()
-                vmss_threads_list.append(stop_vmss_thread)
-                if len(vmss_threads_list) > _MAX_THREADS:
-                    for thread in vmss_threads_list:
-                        thread.join()
-                    del vmss_threads_list[:]
+        vmss_detail = compute_client.virtual_machine_scale_sets.get(
+            group.name, vmss.name, expand="instanceView"
+        )
+        
+        if vmss.instance_view.statuses[1].code == "PowerState/running":
+            stop_vmss_thread = StopVMSSThread(group.name, vmss.name)
+            stop_vmss_thread.start()
+            vmss_threads_list.append(stop_vmss_thread)
+            if len(vmss_threads_list) > _MAX_THREADS:
+                for thread in vmss_threads_list:
+                    thread.join()
+                del vmss_threads_list[:]
 
 # Wait for all threads to complete
 for thread in vmss_threads_list:
     thread.join()
 
-print "Finished stopping all VMSSs"
+print("Finished stopping all VMSSs")
