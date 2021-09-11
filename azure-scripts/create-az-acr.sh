950e40247c74f721206ae7e39de847826c8f24a5
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2021-09-11 22:53:07 +0200

    submission screenshots - AKS cluster

---
+++ azure-scripts/create-az-acr.sh
@@ -0,0 +1,22 @@
+resourceGroup="acdnd-c4-project"
+region="westeurope"
+myAcrName="myacr202106"
+clusterName="udacity-cluster"
+
+az acr create --resource-group $resourceGroup --name $myAcrName --sku Basic
+# Log in to the ACR
+az acr login --name $myAcrName
+# Get the ACR login server name
+# To use the azure-vote-front container image with ACR, the image needs to be tagged
+# with the login server address of your registry.
+# Find the login server address of your registry
+az acr show --name $myAcrName --query loginServer --output table
+# Associate a tag to the local image
+docker tag azure-vote-front:v1 myacr202106.azurecr.io/azure-vote-front:v1
+# Now you will see myacr202106.azurecr.io/azure-vote-front:v1 if you run docker images
+# Push the local registry to remote
+docker push myacr202106.azurecr.io/azure-vote-front:v1
+# Verify if you image is up in the cloud.
+az acr repository list --name myacr202106.azurecr.io --output table
+# Associate the AKS cluster with the ACR
+# az aks update -n $clusterName -g $resourceGroup --attach-acr $myAcrName
--- azure-scripts/create-cluster.sh
+++ azure-scripts/create-cluster.sh
@@ -3,6 +3,8 @@
 # Variables
 resourceGroup="acdnd-c4-project"
 clusterName="udacity-cluster"
+region="westeurope"
+myAcrName="myacr202106"
 
 # Install aks cli
 echo "Installing AKS CLI"
@@ -17,24 +19,26 @@ echo "Step 1 - Creating AKS cluster $clusterName"
 # For users working in their personal Azure account
 # This commmand will not work for the Cloud Lab users, because you are not allowed to create Log Analytics workspace for monitoring
 az aks create \
---resource-group $resourceGroup \
---name $clusterName \
---node-count 1 \
---enable-addons monitoring \
---generate-ssh-keys
+    --resource-group $resourceGroup \
+    --name $clusterName \
+    --node-count 1 \
+    --enable-addons monitoring \
+    --generate-ssh-keys \
+    --location $region \
+    --attach-acr $myAcrName
 
 # For Cloud Lab users
-az aks create \
---resource-group $resourceGroup \
---name $clusterName \
---node-count 1 \
---generate-ssh-keys
+# az aks create \
+#     --resource-group $resourceGroup \
+#     --name $clusterName \
+#     --node-count 1 \
+#     --generate-ssh-keys
 
 # For Cloud Lab users
 # This command will is a substitute for "--enable-addons monitoring" option in the "az aks create"
 # Use the log analytics workspace - Resource ID
 # For Cloud Lab users, go to the existing Log Analytics workspace --> Properties --> Resource ID. Copy it and use in the command below.
-az aks enable-addons -a monitoring -n $clusterName -g $resourceGroup --workspace-resource-id "/subscriptions/6c39f60b-2bb1-4e37-ad64-faaf30beaca4/resourcegroups/cloud-demo-153430/providers/microsoft.operationalinsights/workspaces/loganalytics-153430"
+#az aks enable-addons -a monitoring -n $clusterName -g $resourceGroup --workspace-resource-id "/subscriptions/6c39f60b-2bb1-4e37-ad64-faaf30beaca4/resourcegroups/cloud-demo-153430/providers/microsoft.operationalinsights/workspaces/loganalytics-153430"
 
 echo "AKS cluster created: $clusterName"
 
@@ -43,14 +47,35 @@ echo "AKS cluster created: $clusterName"
 echo "Step 2 - Getting AKS credentials"
 
 az aks get-credentials \
---resource-group $resourceGroup \
---name $clusterName \
---verbose
+    --resource-group $resourceGroup \
+    --name $clusterName \
+    --verbose
 
 echo "Verifying connection to $clusterName"
 
 kubectl get nodes
 
-# echo "Deploying to AKS cluster"
-# The command below will deploy a standard application to your AKS cluster. 
-# kubectl apply -f azure-vote.yaml
+echo "Deploying to AKS cluster"
+The command below will deploy a standard application to your AKS cluster.
+kubectl apply -f ../azure-vote-all-in-one-redis.yaml
+# Test the application at the External IP
+# It will take a few minutes to come alive.
+#kubectl get service azure-vote-front --watch
+# You can also verify that the service is running like this
+#kubectl get service
+# Check the status of each node
+#kubectl get pods
+# Push your local changes to the remote Github repo, preferably in the Deploy_to_AKS branch
+
+#Create an autoscaler by using the following Azure CLI command
+#kubectl autoscale deployment azure-vote-front --cpu-percent=70 --min=1 --max=10
+
+
+##Now, to generate the synthetic load on the AKS cluster, you can run:
+
+# Generate load in the terminal by creating a container with "busybox" image
+# Open the bash into the container
+#kubectl run -it --rm load-generator --image=busybox /bin/sh
+#You will see a new command prompt. Enter the following in the new command prompt. It will send an infinite loop of queries to the cluster and increase the load on the cluster.
+
+#while true; do wget -q -O- 20.93.199.149; done
